ec840a67c0e585394a930370184c06e0cedc083b
Ramesh <ramesh.kec.85@gmail.com>
2022-07-20 21:40:06 +0530

    Images added

--- app/src/main/java/com/macsoftech/vihaan/SplashScreenActivity.java
+++ app/src/main/java/com/macsoftech/vihaan/SplashScreenActivity.java
@@ -4,20 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.macsoftech.vihaan.api.RestApi;
-import com.macsoftech.vihaan.model.BrandListResponse;
-
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-public class MainActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
 //        RestApi.getInstance().getService().brandList().enqueue(new Callback<BrandListResponse>() {
 //            @Override
 //            public void onResponse(Call<BrandListResponse> call, Response<BrandListResponse> response) {
