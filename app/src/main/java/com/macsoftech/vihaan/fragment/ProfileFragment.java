43553f9514697f0a9e117097200cf4b2c9ffd8b1
Ramesh <ramesh.kec.85@gmail.com>
2022-08-04 15:50:59 +0530

    Bug Fixes

--- app/src/main/java/com/macsoftech/vihaan/fragment/ProfileFragment.java
+++ app/src/main/java/com/macsoftech/vihaan/fragment/ProfileFragment.java
@@ -4,19 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.macsoftech.vihaan.BuildConfig;
 import com.macsoftech.vihaan.R;
 
 import org.jetbrains.annotations.NotNull;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ProfileFragment extends BaseFragment {
+    @BindView(R.id.txt_version)
+    TextView txt_version;
+
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         return LayoutInflater.from(getActivity()).inflate(R.layout.frag_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+        txt_version.setText("App Version: " + BuildConfig.VERSION_NAME);
     }
 }
